9b70812a69493fb7ecd5658bb9d4528c430e2381
Noctumbras <ranger.stephen@hotmail.com>
2026-03-12 13:47:25 -0500

    w02 learning activity and small amount of chamber directory setup

---
+++ chamber/scripts/footer.js
@@ -0,0 +1,4 @@
+const date = new Date();
+
+document.getElementById("current-year").innerHTML = `© ${date.getFullYear()} - Stephen Sanders - Illinois, US`;
+document.getElementById("last-modified").innerHTML = 'Last modified: ' + document.lastModified;
