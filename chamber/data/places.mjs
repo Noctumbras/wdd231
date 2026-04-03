04740354f52848c890cb5f5a601f37c43d4fad5a
Noctumbras <ranger.stephen@hotmail.com>
2026-04-03 17:00:29 -0500

    hopefully this is good

--- chamber/data/places.mjs
+++ chamber/data/places.mjs
@@ -1,50 +1,50 @@
 export const places = [
     {
-        "name": "placeholder",
-        "address": "111 placeholder road",
-        "description": "This is a placeholder description for a placeholder place of interest.",
-        "imageURL": "images/apple.webp"
+        "name": "Veteran's Memorial",
+        "address": "131 Cargill Way",
+        "description": "Dedicated to the veterans and featuring five bas relief bronze monuments.",
+        "imageURL": "images/veterans-memorial.webp"
     },
     {
-        "name": "placeholder",
-        "address": "111 placeholder road",
-        "description": "This is a placeholder description for a placeholder place of interest.",
-        "imageURL": "images/apple.webp"
+        "name": "Byerly Bark Dog Park",
+        "address": "701 W Washington St",
+        "description": "Hartsville's Byerly Bark dog park has separate areas for small and large dogs, benches, doggy water fountains, waste stations, and more.",
+        "imageURL": "images/dog-park.webp"
     },
     {
-        "name": "placeholder",
-        "address": "111 placeholder road",
-        "description": "This is a placeholder description for a placeholder place of interest.",
-        "imageURL": "images/apple.webp"
+        "name": "Kalmia Gardens",
+        "address": "1624 W Carolina Ave",
+        "description": "Public botanical garden with woods, trails & diverse plant & animal life, plus educational programs. Open dawn to dusk every day.",
+        "imageURL": "images/kalmia-gardens.webp"
     },
     {
-        "name": "placeholder",
-        "address": "111 placeholder road",
-        "description": "This is a placeholder description for a placeholder place of interest.",
-        "imageURL": "images/apple.webp"
+        "name": "Neptune Island Waterpark",
+        "address": "1109 14th Street",
+        "description": "Inhabited by pirates, mermaids, and endangered oceanic creatures, the island is a lost oasis waiting to be discovered.",
+        "imageURL": "images/neptune-waterpark.webp"
     },
     {
-        "name": "placeholder",
-        "address": "111 placeholder road",
-        "description": "This is a placeholder description for a placeholder place of interest.",
-        "imageURL": "images/apple.webp"
+        "name": "Black Creek Arts Center",
+        "address": "116 W College Ave",
+        "description": "The center of the arts in Darlington County. Offers a variety of arts programs, including arts education classes for adults and children.",
+        "imageURL": "images/black-creek.webp"
     },
     {
-        "name": "placeholder",
-        "address": "111 placeholder road",
-        "description": "This is a placeholder description for a placeholder place of interest.",
-        "imageURL": "images/apple.webp"
+        "name": "The Center Theater",
+        "address": "212 N 5th St",
+        "description": "Built in 1936, Center Theater is a premier performance space in the region, hosting everything from community events and pageants to famous performers.",
+        "imageURL": "images/center-theater.webp"
     },
     {
-        "name": "placeholder",
-        "address": "111 placeholder road",
-        "description": "This is a placeholder description for a placeholder place of interest.",
-        "imageURL": "images/apple.webp"
+        "name": "Black Creek Bistro",
+        "address": "110 W Pinewood Ave",
+        "description": "Owner & Chef Josh Shumate attended the French Culinary Institute in New York City.",
+        "imageURL": "images/bistro.webp"
     },
     {
-        "name": "placeholder",
-        "address": "111 placeholder road",
-        "description": "This is a placeholder description for a placeholder place of interest.",
-        "imageURL": "images/apple.webp"
+        "name": "Fairfield Inn",
+        "address": "200 S Fourth St",
+        "description": "A seven-minute walk from Hartsville Museum and beside Burry park this straightforward hotel is also one mile from Lawton Park and Pavilion.",
+        "imageURL": "images/fairfield-inn.webp"
     },
 ]
